80489f22aab4cc3f693e48fb4e3f46807e1a8c79
diogo.alcon@gmail.com <diogo.alcon@gmail.com>
2023-06-14 16:17:31 -0300

    fix: reorganiza arquivos

--- app/backend/src/Interfaces/IMatches.ts
+++
@@ -1,9 +0,0 @@
-import { Identifiable } from '.';
-
-export interface IMatches extends Identifiable {
-  homeTeamId: number;
-  homeTeamGoals: number;
-  awayTeamId: number;
-  awayTeamGoals: number;
-  inProgress: boolean;
-}
